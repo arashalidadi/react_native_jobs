aa67bde5e5dce1cdeba2d5a33419118e56191e72
Arash@m <alidadiarash71@gmail.com>
2023-06-07 05:59:32 +0430

    create Popularjobs component and add PopularJobCard to it

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PopularJobCard = () => {
+  return (
+    <View>
+      <Text>PopularJobCard</Text>
+    </View>
+  )
+}
+
+export default PopularJobCard;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, ActivityIndicator, FlatList } from 'react-native'
 import React from 'react'
+import styles from './popularjobs.style'
+import { COLORS, SIZES } from '../../../constants';
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+
 
 const Popularjobs = () => {
+  const isLoading = false;
+  const error = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+     <View style={styles.header}>
+       <Text style={styles.headerTitle}>
+         Popular Jobs
+       </Text>
+       <TouchableOpacity>
+        <Text style={styles.headerBtn} >
+        show all
+        </Text>
+       </TouchableOpacity>
+     </View>
+        <View style={styles.cardsContainer}>
+           {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+
+           ) : error ? (
+           <Text>Somting went wrong</Text>
+           ) : (
+            <FlatList 
+            data={[1,2,3,4,5,6,7,8,9]}
+            renderItem={( {item} ) => (
+                <PopularJobCard
+                item={item}
+                />
+            )}
+          keyExtractor={item => item?.job_id}
+          contentContainerStyle={{columnGap : SIZES.medium }}
+          horizontal
+            />
+           )
+          }
+        </View>
+
     </View>
   )
 }
